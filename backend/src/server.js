2d894caaf9a99d788feddd45e508483533a9b11d
TheSkyroo <ishaant69@gmail.com>
2025-12-22 22:57:49 +0530

    auth added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,15 @@
 import express from "express";
-import  "dotenv/config";
-import authRoutes from "./routes/auth.route.js"
+import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
-const app = express()
+const app = express();
 
+const PORT = process.env.PORT;
 
-const PORT=process.env.PORT
+app.use("/api/auth", authRoutes);
 
-
-app.use("/api/auth",authRoutes)
-
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    connectDB();
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
+});
