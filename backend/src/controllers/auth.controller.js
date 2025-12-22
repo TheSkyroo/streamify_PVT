c2abdaa92f4590ddba3b37c05bb1aec40743d277
TheSkyroo <ishaant69@gmail.com>
2025-12-22 22:56:59 +0530

    auth added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,12 +1,9 @@
-export async function signup(req,res){
-    res.send('signup Routes');
-
+export async function signup(req, res) {
+  res.send("signup Routes");
 }
-export async function login(req,res){
-    res.send('login Routes');
-
+export async function login(req, res) {
+  res.send("login Routes");
 }
-export function logout(req,res){
-    res.send('logout Routes');
-
+export function logout(req, res) {
+  res.send("logout Routes");
 }
