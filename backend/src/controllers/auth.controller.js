bbcd9efe7fc0ec2b4d1b787e5dd1200a5f127439
TheSkyroo <ishaant69@gmail.com>
2025-12-22 22:55:23 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export async function signup(req,res){
+    res.send('signup Routes');
+
+}
+export async function login(req,res){
+    res.send('login Routes');
+
+}
+export function logout(req,res){
+    res.send('logout Routes');
+
+}
