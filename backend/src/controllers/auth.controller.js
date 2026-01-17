b384afcf1f3491366928be681d711e8278b8c602
TheSkyroo <ishaant69@gmail.com>
2026-01-17 23:04:58 +0530

    restart

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -79,7 +79,7 @@ export async function signup(req, res) {
       httpOnly: true,//prevent XSS attack,
       sameSite:"strict",//prevent CSRF attacks,
       secure:process.env.NODE_ENV==="production",
-    })
+    });
 
     res.status(201).json({success:true,user:newUser})
 
@@ -94,16 +94,39 @@ export async function login(req, res) {
   try{
     const{email,password}=req.body;
     if(!email || !password){
-      return res.status(400).json({message:"All fields are required  "});
+      return res.status(400).json({message:"All fields are required"});
 
     }
+    const user =await User.findOne({email});
+    if(!user) return res.status(401).json({message:"Invalid email or password"});
+    const isPasswordCorrect = await user.matchPassword(password)
+    if(!isPasswordCorrect) return res.status(401).json({message:"Invalid email or password"})
+
     
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    )
+    res.cookie("jwt",token,{
+      maxAge:7*24*60*60*1000,
+      httpOnly: true,//prevent XSS attack,
+      sameSite:"strict",//prevent CSRF attacks,
+      secure:process.env.NODE_ENV==="production",
+    });
+    res.status(200).json({success:true,user})
+
 
   }
   catch(error){
+    console.log("error in login controler",error.message);
+    res.status(500).json({message:"internal Server Error"});
 
   }
 }
 export function logout(req, res) {
-  res.send("logout Routes");
+  res.clearCookie("jwt")
+  res.status(200).json({success:true,message:"logout successful"});
 }
