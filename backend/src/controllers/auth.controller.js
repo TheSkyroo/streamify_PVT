65bb92953f05fcaf42c67c91c61f078daab1b1b5
TheSkyroo <ishaant69@gmail.com>
2026-01-18 06:55:13 +0530

    restart

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ export async function signup(req, res) {
         .json({ message: "Email already exists please use a diffrent one" });
     }
 
-    //avtar start (may change in future) 
+    //avtar start (may change in future)
 
     const avtar = [
       "Riley",
@@ -65,82 +65,66 @@ export async function signup(req, res) {
       password,
       profilePic: genAvtar,
     });
-    try{
-      
-    await upsertStreamUser({
-      id:newUser._id.toString(),
-      name:newUser.fullName,
-      image:newUser.profilePic || " ",
-    });
-    console.log(`Stream user created for ${newUser.fullName} `);
-
-    }catch(error){
-      console.log("Error creating Stream user:",error);
-
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || " ",
+      });
+      console.log(`Stream user created for ${newUser.fullName} `);
+    } catch (error) {
+      console.log("Error creating Stream user:", error);
     }
-
-
 
     const token = jwt.sign(
       { userId: newUser._id },
       process.env.JWT_SECRET_KEY,
       {
         expiresIn: "7d",
-      }
-    )
-    res.cookie("jwt",token,{
-      maxAge:7*24*60*60*1000,
-      httpOnly: true,//prevent XSS attack,
-      sameSite:"strict",//prevent CSRF attacks,
-      secure:process.env.NODE_ENV==="production",
+      },
+    );
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevent XSS attack,
+      sameSite: "strict", //prevent CSRF attacks,
+      secure: process.env.NODE_ENV === "production",
     });
 
-    res.status(201).json({success:true,user:newUser})
-
-
-
+    res.status(201).json({ success: true, user: newUser });
   } catch (error) {
-    console.log("Error in signup controller",error);
-    res.status(500).json({message:"Internal Server Error"});
+    console.log("Error in signup controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 export async function login(req, res) {
-  try{
-    const{email,password}=req.body;
-    if(!email || !password){
-      return res.status(400).json({message:"All fields are required"});
-
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    const user =await User.findOne({email});
-    if(!user) return res.status(401).json({message:"Invalid email or password"});
-    const isPasswordCorrect = await user.matchPassword(password)
-    if(!isPasswordCorrect) return res.status(401).json({message:"Invalid email or password"})
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(401).json({ message: "Invalid email or password" });
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect)
+      return res.status(401).json({ message: "Invalid email or password" });
 
-    
-    const token = jwt.sign(
-      { userId: user._id },
-      process.env.JWT_SECRET_KEY,
-      {
-        expiresIn: "7d",
-      }
-    )
-    res.cookie("jwt",token,{
-      maxAge:7*24*60*60*1000,
-      httpOnly: true,//prevent XSS attack,
-      sameSite:"strict",//prevent CSRF attacks,
-      secure:process.env.NODE_ENV==="production",
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
     });
-    res.status(200).json({success:true,user})
-
-
-  }
-  catch(error){
-    console.log("error in login controler",error.message);
-    res.status(500).json({message:"internal Server Error"});
-
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevent XSS attack,
+      sameSite: "strict", //prevent CSRF attacks,
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("error in login controler", error.message);
+    res.status(500).json({ message: "internal Server Error" });
   }
 }
 export function logout(req, res) {
-  res.clearCookie("jwt")
-  res.status(200).json({success:true,message:"logout successful"});
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "logout successful" });
 }
