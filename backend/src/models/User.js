d460f9361bc36de7b467affa6359d78f4ddafbc1
TheSkyroo <ishaant69@gmail.com>
2026-01-18 06:54:55 +0530

    restart

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -48,10 +48,8 @@ const userSchema = new mongoose.Schema(
       },
     ],
   },
-  { timestamps: true }
+  { timestamps: true },
 );
-
-
 
 //pre hook
 
@@ -66,11 +64,13 @@ userSchema.pre("save", async function (next) {
     next(error);
   }
 });
-userSchema.methods.matchPassword=async function (enteredPassword) {
-  const isPasswordCorrect = await bcrypt.compare(enteredPassword,this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  const isPasswordCorrect = await bcrypt.compare(
+    enteredPassword,
+    this.password,
+  );
   return isPasswordCorrect;
-  
-}
+};
 const User = mongoose.model("User", userSchema);
 
 export default User;
