3876134a1e996f4c8bbb893b5637b5163ba02327
TheSkyroo <ishaant69@gmail.com>
2026-01-19 07:05:50 +0530

    restart

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { login, signup, logout } from "../controllers/auth.controller.js"
+import { login, signup, logout,onboard } from "../controllers/auth.controller.js"
+import {protectedRoute} from "../middleware/auth.middlewars.js"
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post("/login", login)
 router.post("/logout", logout)
+
+router.post("/onboarding",protectedRoute,onboard)
 
 export default router
