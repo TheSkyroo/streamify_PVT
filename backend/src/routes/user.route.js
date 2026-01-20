073c50080f97d56d3025c8a21b9f7c8dfc4f3d53
TheSkyroo <ishaant69@gmail.com>
2026-01-20 19:43:58 +0530

    20 jan

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middlewars.js";
-import {getRecommendedUsers,getMyFriends} from "../controllers/user.cotroller.js"
+import {getRecommendedUsers,getMyFriends,sendFriendRequest} from "../controllers/user.cotroller.js"
 const router=express.Router();
 
 router.use(protectRoute);
 
-router.get("/",getRecommendedUsers)
-router.get("/friends",getMyFriends)
+router.get("/",getRecommendedUsers);
+router.get("/friends",getMyFriends);
+
+router.post("/friend-request/:id",sendFriendRequest);
 
 export default router;
