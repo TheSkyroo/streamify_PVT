865485938d32ddc50a57eded22bcd314d3878fb4
TheSkyroo <ishaant69@gmail.com>
2026-01-20 05:38:53 +0530

    20 jan

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
-
 const app = express();
-
 
 const PORT = process.env.PORT;
 app.use(express.json());
