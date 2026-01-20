5ea877c32ceabb63c3560b6fbaaf702667ddd342
TheSkyroo <ishaant69@gmail.com>
2026-01-20 06:01:25 +0530

    20 jan

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,13 +5,19 @@ import {
   logout,
   onboard,
 } from "../controllers/auth.controller.js";
-import { protectedRoute } from "../middleware/auth.middlewars.js";
+import { protectRoute } from "../middleware/auth.middlewars.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", logout);//logout is post becouse it changes the state of server  
 
-router.post("/onboarding", protectedRoute, onboard);
+
+//check i user is aunthaticated or not
+router.get("/me",protectRoute,(req,res)=>{
+  res.status(200).json({success:true,user:req.user});
+})
+
+router.post("/onboarding", protectRoute, onboard);
 
 export default router;
