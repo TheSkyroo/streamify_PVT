fae36dc852ab2376cd33afcf5e63c0155e835b04
TheSkyroo <ishaant69@gmail.com>
2026-01-20 21:52:39 +0530

    backend

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,15 +1,22 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middlewars.js";
-import {getRecommendedUsers,getMyFriends,sendFriendRequest,acceptFriendRequest,getFriendRequests,getOutgoingFriendReqs} from "../controllers/user.cotroller.js"
-const router=express.Router();
+import {
+  getRecommendedUsers,
+  getMyFriends,
+  sendFriendRequest,
+  acceptFriendRequest,
+  getFriendRequests,
+  getOutgoingFriendReqs,
+} from "../controllers/user.cotroller.js";
+const router = express.Router();
 
 router.use(protectRoute);
 
-router.get("/",getRecommendedUsers);
-router.get("/friends",getMyFriends);
+router.get("/", getRecommendedUsers);
+router.get("/friends", getMyFriends);
 
-router.post("/friend-request/:id",sendFriendRequest);
-router.put("/friend-request/:id/accept",acceptFriendRequest);
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 router.get("/friend-requests", getFriendRequests);
 router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
