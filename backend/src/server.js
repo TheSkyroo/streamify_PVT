2b1b035527cd1392efabec8262dffb0c4070231e
TheSkyroo <ishaant69@gmail.com>
2026-04-09 10:11:43 +0530

    deplow

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ if (!process.env.JWT_SECRET_KEY) {
   process.exit(1);
 }
 import cors from "cors";
-
+import path from "path";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -15,19 +15,26 @@ import { connectDB } from "./lib/db.js";
 
 const app = express();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
 
 
-const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectDB();
